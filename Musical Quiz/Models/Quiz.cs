37ac88203176a09afdea231d2a2166442834c3b4
CodeThales <thalles.agency@gmail.com>
2021-11-30 17:05:42 -0300

    <fix>[Quiz Model]: <Insert DataAnnotations>

--- Musical Quiz/Models/Quiz.cs
+++ Musical Quiz/Models/Quiz.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Musical_Quiz.Models
 {
     public class Quiz
     {
+        [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Escolha o estilo musical")]
         public string Theme { get; set; }
     }
 }
