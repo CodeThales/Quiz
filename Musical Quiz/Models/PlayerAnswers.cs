9f641d87e9c4998401d24c23921171275ac109ed
CodeThales <thalles.agency@gmail.com>
2021-11-30 17:05:15 -0300

    <fix>[PlayerAnswers Model]: <Insert DataAnnotations>

--- Musical Quiz/Models/PlayerAnswers.cs
+++ Musical Quiz/Models/PlayerAnswers.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Musical_Quiz.Models
 {
     public class PlayerAnswers
     {
+        [Key]
         public int Id { get; set; }
-        public int QuestionId { get; set; }
+
+        [ForeignKey("Player")]
+        [Column(Order = 1)]
         public int PlayeId { get; set; }
-        public int OptionsId { get; set; }
+
+        [ForeignKey("Question")]
+        [Column(Order = 2)]
+        public int QuestionId { get; set; }
+
+        [ForeignKey("Option")]
+        [Column(Order = 2)]
+        public int OptionId { get; set; }
     }
 }
