c4b0658bd44358b1c50ec1bbac875a6dc1adb483
CodeThales <thalles.agency@gmail.com>
2021-11-30 17:06:41 -0300

    <feat>[Program]: <Configure Program for SeedDatabase>

--- Musical Quiz/Program.cs
+++ Musical Quiz/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Musical_Quiz.Data;
 
 namespace Musical_Quiz
 {
@@ -13,7 +10,15 @@ namespace Musical_Quiz
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<Context>();
+                context.Database.Migrate();
+                SeedDatabase.Initialize(services);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
