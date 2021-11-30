84384d2b21b0946e50365c6dfd8cb37dceb8c4e0
CodeThales <thalles.agency@gmail.com>
2021-11-30 17:04:48 -0300

    <fix>[Player Model]: <Insert DataAnnotations>

--- Musical Quiz/Models/Player.cs
+++ Musical Quiz/Models/Player.cs
@@ -1,22 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Musical_Quiz.Models
 {
     public class Player
     {
+        [Key]
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public int Points { get; set; }
 
-        public DateTime? created { get; set; }
-        public DateTime? updated { get; set; }
-        public string updatedById { get; set; }
-        public IdentityUser updatedBy { get; set; }
-        public string createdById { get; set; }
-        public IdentityUser createdBy { get; set; }
+        [Display(Name ="Nome do jogador")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [MaxLength(10, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Sobrenome do jogador")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [MaxLength(10, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
+        public string LastName { get; set; }
+        public int Points { get; set; }        
     }
 }
