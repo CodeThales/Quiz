f800d0aaa554bb34f7109c9afaadf238f1714621
CodeThales <thalles.agency@gmail.com>
2021-12-01 20:58:01 -0300

    <feat>[controller]:<create QuizController>

--- Musical Quiz/Controllers/QuizController.cs
+++ Musical Quiz/Controllers/QuizController.cs
@@ -1,11 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Musical_Quiz.Models;
+using Musical_Quiz.Services;
 
 namespace Musical_Quiz.Controllers
 {
-    public class QuizController
+    [ApiController]
+    [Route("[controller]")]
+    public class QuizController : APIBaseController
     {
+        IQuizService _service;
+
+        public QuizController(IQuizService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Index() => ApiOk(_service.All());
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Index(int? id)
+        {
+            if (id <= 0 || id > _service.QuizList())
+                return ApiNotFound("Não encontramos este tema em nosso banco de dados.");
+            else return ApiOk(_service.Get(id));
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Quiz quiz)
+        {
+            return _service.Create(quiz) ?
+                ApiOk(quiz, "Tema cadastrado com sucesso.") :
+                ApiNotFound("Erro ao tentar cadastrar este tema.");
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Quiz quiz)
+        {
+            return _service.Update(quiz) ?
+                ApiOk(quiz, "Tema atualizado com sucesso.") :
+                ApiNotFound("Erro ao tentar atualizar este tema.");
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete(int? id) =>
+            _service.Delete(id) ?
+            ApiOk("Tema excluído com sucesso.") :
+            ApiNotFound("Erro ao tentar excluir este tema.");
     }
 }
