7fc52f9f529b87debd7df336cb95b167dbc7d9b7
CodeThales <thalles.agency@gmail.com>
2021-12-01 09:16:45 -0300

    <feat>[services]: <Create Option Service Interface>

---
+++ Musical Quiz/Services/IOptionService.cs
@@ -0,0 +1,15 @@
+﻿using Musical_Quiz.Models;
+using System.Collections.Generic;
+
+namespace Musical_Quiz.Services
+{
+    public interface IOptionService
+    {
+        List<Option> All();
+        Option Get(int? id);
+        bool Create(Option option);
+        bool Update(Option option);
+        bool Delete(int? id);
+
+    }
+}
