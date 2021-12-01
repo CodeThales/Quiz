98efd2e6ddc487d2f73b2e5a1a78c34eeabba954
CodeThales <thalles.agency@gmail.com>
2021-12-01 10:34:58 -0300

    <feat>[APIResponse]: <Create API Response Class>

--- Musical Quiz/API/APIResponse.cs
+++ Musical Quiz/API/APIResponse.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Musical_Quiz.API
+﻿namespace Musical_Quiz.API
 {
-    public class APIResponse
+    public class APIResponse<T>
     {
+        public bool Succeed { get; set; }
+        public string Message { get; set; }
+        public T Results { get; set; }
     }
 }
