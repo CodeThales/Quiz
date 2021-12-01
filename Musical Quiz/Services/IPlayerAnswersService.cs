9c788d391434c647139bd9a0bc4d4dd02a0f8235
CodeThales <thalles.agency@gmail.com>
2021-12-01 09:17:35 -0300

    <feat>[services]: <Create PlayerAnswers Service Interface>

---
+++ Musical Quiz/Services/IPlayerAnswersService.cs
@@ -0,0 +1,15 @@
+﻿using Musical_Quiz.Models;
+using System.Collections.Generic;
+
+namespace Musical_Quiz.Services
+{
+    public interface IPlayerAnswersService
+    {
+        List<PlayerAnswers> All();
+        PlayerAnswers Get(int? id);
+        bool Create(PlayerAnswers playerAnswers);
+        bool Update(PlayerAnswers playerAnswers);
+        bool Delete(int? id);
+
+    }
+}
