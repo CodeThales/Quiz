fef9a1408205edf49faf9029d1e089e37ab014f2
CodeThales <thalles.agency@gmail.com>
2021-12-01 10:23:41 -0300

    <feat>[QuestionService]: <CreateQuestionService with CRUD>

--- Musical Quiz/Services/QuestionService.cs
+++ Musical Quiz/Services/QuestionService.cs
@@ -1,11 +1,85 @@
-﻿using System;
+﻿using Musical_Quiz.Data;
+using Musical_Quiz.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Musical_Quiz.Services
 {
-    public class QuestionService
+    public class QuestionService : IQuestionService
     {
+        Context _context;
+
+        public QuestionService(Context context)
+        {
+            _context = context;
+        }
+
+        public List<Question> All()
+        {
+            if (!_context.Question.Any())
+                throw new Exception("Não encontramos perguntas cadastradas em nosso banco de dados.");
+            return _context.Question.ToList();
+        }
+
+        public Question Get(int? id)
+        {
+            try
+            {
+                return _context.Question.FirstOrDefault(q => q.Id == id);
+            }
+            catch
+            {
+                throw new Exception("Pergunta não encontrada em nosso banco de dados.");
+            }
+        }
+
+        public bool Create(Question question)
+        {
+            try
+            {
+                _context.Add(question);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Update(Question question)
+        {
+            try
+            {
+                if (!_context.Question.Any(q => q.Id == question.Id))
+                    throw new Exception("Não encontramos esta pergunta em nosso banco de dados.");
+
+                _context.Update(question);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int? id)
+        {
+            try
+            {
+                if (!_context.Question.Any(q => q.Id == id))
+                    throw new Exception("Não encontramos esta pergunta em nosso banco de dados.");
+
+                _context.Remove(this.Get(id));
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
