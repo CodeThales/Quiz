e2831eb8959b4f273dbcc12205b6b18b9597ddd2
CodeThales <thalles.agency@gmail.com>
2021-12-01 09:18:05 -0300

    <feat>[services]: <Create Question Service Interface>

---
+++ Musical Quiz/Services/IQuestionService.cs
@@ -0,0 +1,15 @@
+﻿using Musical_Quiz.Models;
+using System.Collections.Generic;
+
+namespace Musical_Quiz.Services
+{
+    public interface IQuestionService
+    {
+        List<Question> All();
+        Question Get(int? id);
+        bool Create(Question question);
+        bool Update(Question question);
+        bool Delete(int? id);
+
+    }
+}
