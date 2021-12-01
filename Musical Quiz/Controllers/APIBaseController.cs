931fef7daa410f537c804d5019d207fcd75ea425
CodeThales <thalles.agency@gmail.com>
2021-12-01 11:01:19 -0300

    <feat>[Controllers/ApiBaseController]:<Create ApiBaseController>

--- Musical Quiz/Controllers/APIBaseController.cs
+++ Musical Quiz/Controllers/APIBaseController.cs
@@ -1,11 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Musical_Quiz.API;
 
 namespace Musical_Quiz.Controllers
 {
-    public class APIBaseController
+    public abstract class APIBaseController : ControllerBase
     {
+        #region PrivateMethods
+        APIResponse<T> CustomResponse<T>(T Results, bool Succeed = true, string Message = "") =>
+            new APIResponse<T>()
+            {
+                Results = Results,
+                Succeed = Succeed,
+                Message = Message
+            };
+
+        APIResponse<string> CustomResponse(bool Succeed = true, string Message = "") =>
+            new APIResponse<string>()
+            {
+                Succeed = Succeed,
+                Message = Message
+            };
+        #endregion PrivateMethods
+
+        protected OkObjectResult ApiOk<T>(T Results, string Message = "") =>
+          Ok(CustomResponse(Results, true, Message));
+
+        protected OkObjectResult ApiOk(string Message = "") =>
+            Ok(CustomResponse(true, Message));
+
+        protected NotFoundObjectResult ApiNotFound(string Message) =>
+            NotFound(CustomResponse(false, Message));
+
+        protected BadRequestObjectResult ApiBadRequest<T>(T Results, string Message = "") =>
+            BadRequest(CustomResponse(Results, false, Message));
     }
 }
