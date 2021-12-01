368aeca0fe89277fe4d888d344dc90b1a001a362
CodeThales <thalles.agency@gmail.com>
2021-12-01 13:47:29 -0300

    <feat>[OptionController]:<Create OptionController>
    Create endpoints:
    GET, POST, PUT and DELETE

--- Musical Quiz/Controllers/OptionController.cs
+++ Musical Quiz/Controllers/OptionController.cs
@@ -1,11 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Musical_Quiz.Models;
+using Musical_Quiz.Services;
 
 namespace Musical_Quiz.Controllers
 {
-    public class OptionController
+    [ApiController]
+    [Route("[controller]")]
+    public class OptionController : APIBaseController
     {
+        IOptionService _service;
+
+        public OptionController(IOptionService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Index() => ApiOk(_service.All());
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Index(int? id) => ApiOk(_service.Get(id));
+        
+        [HttpPost]
+        public IActionResult Create([FromBody] Option option)
+        {
+            return _service.Create(option) ?
+                ApiOk(option, "Alternativa cadastrada com sucesso.") :
+                ApiNotFound("Erro ao tentar cadastrar alternativa.");
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Option option)
+        {
+            return _service.Update(option) ?
+                ApiOk(option, "Alternativa atualizada com sucesso.") :
+                ApiNotFound("Erro ao tentar atualizar alternativa.");
+        }
+
+        [HttpDelete]        
+        [Route("{id}")]
+        public IActionResult Delete(int? id) =>
+            _service.Delete(id) ?
+            ApiOk("Alternativa excluída com sucesso.") :
+            ApiNotFound("Erro ao excluir alternativa.");
+
     }
 }
