e8c62caf2132e346ce1519227d2f3ca8543d17b6
CodeThales <thalles.agency@gmail.com>
2021-12-01 10:23:55 -0300

    <feat>[QuizService]: <Create QuizService with CRUD>

--- Musical Quiz/Services/QuizService.cs
+++ Musical Quiz/Services/QuizService.cs
@@ -1,11 +1,84 @@
-﻿using System;
+﻿using Musical_Quiz.Data;
+using Musical_Quiz.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Musical_Quiz.Services
 {
-    public class QuizService
+    public class QuizService : IQuizService
     {
+        Context _context;
+
+        public QuizService(Context context)
+        {
+            _context = context;
+        }
+        public List<Quiz> All()
+        {
+            if (!_context.Quiz.Any())
+                throw new Exception("Não encontramos temas cadastrados em nosso banco de dados.");
+            return _context.Quiz.ToList();
+        }
+
+        public Quiz Get(int? id)
+        {
+            try
+            {
+                return _context.Quiz.FirstOrDefault(q => q.Id == id);
+            }
+            catch
+            {
+                throw new Exception("Tema não encontrado em nosso banco de dados.");
+            }
+        }
+
+        public bool Create(Quiz quiz)
+        {
+            try
+            {
+                _context.Add(quiz);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Update(Quiz quiz)
+        {
+            try
+            {
+                if (!_context.Quiz.Any(q => q.Id == quiz.Id))
+                    throw new Exception("Não encontramos este tema em nosso banco de dados.");
+
+                _context.Update(quiz);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int? id)
+        {
+            try
+            {
+                if (!_context.Quiz.Any(q => q.Id == id))
+                    throw new Exception("Não encontramos este tema em nosso banco de dados.");
+
+                _context.Remove(this.Get(id));
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
