0e0b79b26c2f54395591828d2bacf1b5653dd5fe
CodeThales <thalles.agency@gmail.com>
2021-12-07 10:20:27 -0300

    <feat>[QuizController]:<Creating comments for all quiz theme controller endpoints>

--- Musical Quiz/Controllers/QuizController.cs
+++ Musical Quiz/Controllers/QuizController.cs
@@ -15,9 +15,20 @@ namespace Musical_Quiz.Controllers
             _service = service;
         }
 
+
+        /// <summary>
+        /// Ruturs a list of all quiz themes.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Index() => ApiOk(_service.All());
 
+
+        /// <summary>
+        /// Returns a quiz theme fetched by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet]
         [Route("{id}")]
         public IActionResult Index(int? id)
@@ -27,6 +38,12 @@ namespace Musical_Quiz.Controllers
             else return ApiOk(_service.Get(id));
         }
 
+
+        /// <summary>
+        /// Creates a new quiz theme.
+        /// </summary>
+        /// <param name="quiz"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult Create([FromBody] Quiz quiz)
         {
@@ -35,6 +52,12 @@ namespace Musical_Quiz.Controllers
                 ApiNotFound("Erro ao tentar cadastrar este tema.");
         }
 
+
+        /// <summary>
+        /// Updates a quiz theme.
+        /// </summary>
+        /// <param name="quiz"></param>
+        /// <returns></returns>
         [HttpPut]
         public IActionResult Update([FromBody] Quiz quiz)
         {
@@ -43,6 +66,12 @@ namespace Musical_Quiz.Controllers
                 ApiNotFound("Erro ao tentar atualizar este tema.");
         }
 
+
+        /// <summary>
+        /// Delete a quiz theme.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete]
         [Route("{id}")]
         public IActionResult Delete(int? id) =>
